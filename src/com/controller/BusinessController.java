099122981a495961940ce1dced6fe1aa0ffa312b
Santiago Velandia <santiago.velandiag@uqvirtual.edu.co>
2022-05-09 16:57:05 -0500

    Se modifican algunas interfaces

--- src/com/controller/BusinessController.java
+++ src/com/controller/BusinessController.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.entities;
+package com.controller;
 
 /**
  *
  * @author EQUIPO
  */
-public class EShop_BD {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class BusinessController {
     
 }
